94f1e28ce6cc01cc6082e7966afaed332ff05119
Greg3883 <gregoire.bejjaji@gmail.com>
2017-04-19 00:02:40 +0200

    Remise à jour v2

--- src/main/commutel/InterfaceClient.java
+++ src/main/commutel/InterfaceClient.java
@@ -20,9 +20,6 @@ public class InterfaceClient {
 		PrintStream os = null;
 		DataInputStream inputLine = null;
 
-		/*
-		 * Open a socket on port 2222. Open the input and the output streams.
-		 */
 		try {
 			clientSocket = new Socket("localhost", 5000);
 			os = new PrintStream(clientSocket.getOutputStream());
@@ -34,17 +31,9 @@ public class InterfaceClient {
 			System.err.println("Couldn't get I/O for the connection to host");
 		}
 
-		/*
-		 * If everything has been initialized then we want to write some data to
-		 * the socket we have opened a connection to on port 2222.
-		 */
 		if (clientSocket != null && os != null && is != null) {
 			try {
 
-				/*
-				 * Keep on reading from/to the socket till we receive the "Ok"
-				 * from the server, once we received that then we break.
-				 */
 				System.out.println("The client started. Type any text. To quit it type 'Ok'.");
 				String responseLine;
 				os.println(inputLine.readLine());
@@ -56,10 +45,6 @@ public class InterfaceClient {
 					os.println(inputLine.readLine());
 				}
 
-				/*
-				 * Close the output stream, close the input stream, close the
-				 * socket.
-				 */
 				os.close();
 				is.close();
 				clientSocket.close();
